5f0790163d2cd052028c7e17643c2d3f01c320af
Alan Orth <alan.orth@gmail.com>
2021-08-23 11:03:05 +0300

    src: Use TestBed.inject instead of TestBed.get
    As of Angular 9 it is recommended to use TestBed.inject instead of
    TestBed.get. It has the same behavior, but is type safe.
    
    See: https://v9.angular.io/guide/updating-to-version-9

--- frontend/src/app/explorer/services/itemsService/items.service.spec.ts
+++ frontend/src/app/explorer/services/itemsService/items.service.spec.ts
@@ -5,7 +5,7 @@ describe('ItemsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ItemsService = TestBed.get(ItemsService);
+    const service: ItemsService = TestBed.inject(ItemsService);
     expect(service).toBeTruthy();
   });
 });
