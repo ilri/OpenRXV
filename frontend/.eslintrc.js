0b69c5ea0311120a184ce297ee5624ddabbe0648
Alan Orth <alan.orth@gmail.com>
2023-07-08 16:50:52 +0300

    frontend/.eslintrc.js: fix parser fail

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "sourceType": "module"
+        "sourceType": "module",
+        "project": ["./tsconfig.json"]
     },
     "plugins": [
         "eslint-plugin-import",
