f7953a943450da88a5231bb210350e55a52d0a4a
Alan Orth <alan.orth@gmail.com>
2023-07-09 21:27:02 +0300

    backend: reconfigure eslint and prettier
    Use simplified stock eslint configuration for Typescript, with the
    prettier extends last.
    
    See: https://typescript-eslint.io/getting-started
    See: https://github.com/prettier/eslint-config-prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,22 +1,7 @@
+/* eslint-env node */
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint'],
-  extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    'no-unused-vars':1
-  },
 };
