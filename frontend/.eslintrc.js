93dfbfcc999f449f6c333bddd3b38778fd1716e0
Alan Orth <alan.orth@gmail.com>
2023-07-11 08:44:00 +0300

    frontend: remove unused imports with ESLint
    See: https://simondosda.github.io/posts/2021-05-10-eslint-imports.html

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,6 +2,19 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   root: true,
+  'rules': {
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_'
+      }
+    ]
+  }
 };
